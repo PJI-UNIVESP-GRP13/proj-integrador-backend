52ed1b8d2acfdcacb54fb3b1db9cd9547ab04709
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2024-04-06 17:17:33 -0300

    feat: jwt and security configurations

--- src/main/java/com/br/apicerquilhotodos/repository/UserRepository.java
+++ src/main/java/com/br/apicerquilhotodos/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.br.apicerquilhotodos.repository;
 import com.br.apicerquilhotodos.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
