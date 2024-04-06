15cd7a7f4a9058283cd2311a38ada904ef357a82
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2024-04-06 10:28:21 -0300

    feat: initial commit

---
+++ src/main/java/com/br/apicerquilhotodos/ApiCerquilhoTodosApplication.java
@@ -0,0 +1,13 @@
+package com.br.apicerquilhotodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCerquilhoTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCerquilhoTodosApplication.class, args);
+	}
+
+}
