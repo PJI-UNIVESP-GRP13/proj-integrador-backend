7047ce9403a2ba52b0af502b6bc7f60467743246
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2024-04-18 21:16:24 -0300

    feat: add cookie to auth token

--- src/main/java/com/br/apicerquilhotodos/controller/AuthController.java
+++ src/main/java/com/br/apicerquilhotodos/controller/AuthController.java
@@ -6,6 +6,8 @@ import com.br.apicerquilhotodos.dto.RegisterRequestDTO;
 import com.br.apicerquilhotodos.dto.ResponseDTO;
 import com.br.apicerquilhotodos.infra.security.TokenService;
 import com.br.apicerquilhotodos.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,11 +27,30 @@ public class AuthController {
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
 
+//    @PostMapping("/login")
+//    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
+//        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found!"));
+//        if (passwordEncoder.matches(body.password(), user.getPassword())) {
+//            String token = this.tokenService.generateToken(user);
+//            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+//        }
+//        return ResponseEntity.badRequest().build();
+//    }
+
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
+    public ResponseEntity login(@RequestBody LoginRequestDTO body, HttpServletResponse response) {
         User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found!"));
         if (passwordEncoder.matches(body.password(), user.getPassword())) {
             String token = this.tokenService.generateToken(user);
+
+            // Adiciona o token como um cookie na resposta
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true); // Impede que o cookie seja acessado por JavaScript
+            cookie.setMaxAge(60 * 60 * 24 * 7); // Define a validade do cookie (7 dias)
+            cookie.setPath("/"); // Define o caminho do cookie como a raiz do domínio
+            response.addCookie(cookie);
+
+            // Retorna a resposta com o token no corpo
             return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
         }
         return ResponseEntity.badRequest().build();
